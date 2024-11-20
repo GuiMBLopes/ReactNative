94bbf0c743e8d5e16c6f45586fe750a2cce0beb0
Carolina Menezes <caroll.menezes16@gmail.com>
2024-11-19 22:32:28 -0300

    Login

---
+++ vacinaFacil/src/components/ButtonType/style.ts
@@ -0,0 +1,17 @@
+import { StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+ styleButton: {
+    width: "80%",
+   height: 37,
+    borderRadius: 12,
+     alignItems: "center",
+   justifyContent: "center",
+  
+}, 
+
+   text: {
+    color: "#fff",
+     fontSize: 30, 
+   },
+});
