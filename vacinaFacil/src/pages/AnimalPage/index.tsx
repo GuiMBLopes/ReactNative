6be5f326b89dff36d212aae70569d603e9cff09c
Gouiid <guimbl@hotmail.com>
2024-11-20 16:52:08 -0300

    FlatList pagina animal

--- vacinaFacil/src/pages/AnimalPage/index.tsx
+++ vacinaFacil/src/pages/AnimalPage/index.tsx
@@ -1,34 +1,45 @@
-import { Image, Text, View } from "react-native";
+import { FlatList, Image, Text, View } from "react-native";
 import { style } from "./style";
 import { Icon } from "react-native-elements";
+import { DataCardPage } from "../../@types/api";
+import { useEffect, useState } from "react";
+import { CardAnimal } from "../../components/CardAnimal";
+import { listar } from "../../services/serviceApi";
 
 export const AnimalPage = () => {
+  const [info, setInfo] = useState<DataCardPage[]>([]);
 
+  const carregarDados = async () => {
+    const dados = await listar();
+    setInfo(dados);
+  };
+
+  useEffect(() => {
+    carregarDados();
+  }, []);
 
   return (
     <>
-        <View style={style.container}>
-          <View style={style.card}>
-            <View style={style.cardText}>
-              <Text style={style.title}>titulo</Text>
-              <Text style={style.subTitle}>Descrição</Text>
-            </View>
-            <View style={style.local}>
-                <Icon name="location-sharp" type="ionicon"/>
-              <Text style={style.localText}>Rua do Imperador, 15 - Centro</Text>
-            </View>
-            <View style={style.row}>
-                <View style={style.rowDate}>
-                    <Icon name="calendar" type="font-awesome-5"/>
-                    <Text style={style.rowDateText}>10/11/2024 - 10/12/2024</Text>
-                </View>
-                <View style={style.rowTime}>
-                    <Icon name="clock" type="font-awesome-5"/>
-                    <Text style={style.rowTimeText}>8H - 16H</Text>
-                </View>
-            </View>
-          </View>
-        </View>
+      <View style={style.container}>
+        <FlatList
+          data={info}
+          keyExtractor={(info) => info.id}
+          renderItem={({ item }) => (
+            <CardAnimal
+              bairro={item.bairro}
+              dataFim={item.dataFim}
+              dataInicio={item.dataInicio}
+              descricao={item.descricao}
+              horaFim={item.horaFim}
+              horaInicio={item.horaInicio}
+              id={item.id}
+              numero={item.numero}
+              rua={item.rua}
+              tipo={item.tipo}
+            />
+          )}
+        />
+      </View>
     </>
   );
 };
--- vacinaFacil/src/services/serviceApi.ts
+++ vacinaFacil/src/services/serviceApi.ts
@@ -1,21 +1,5 @@
 import axios from "axios";
-import { Cards } from "../@types/api";
-
-interface PostData {
-  rua: string;
-  numero: string;
-  bairro: string;
-  cep: string;
-  descricao: string;
-  dataInicio: string;
-  dataFim: string;
-  horaInicio: string;
-  horaFim: string;
-  tipo: string;
-}
-
-
-
+import { Cards, DataCardPage, PostData } from "../@types/api";
 
 export const inserir = async (data: PostData) => {
   try {
@@ -36,32 +20,60 @@ export const buscar = async () => {
     );
     console.log("Resposta API", response.data);
     const dadosGet = response.data;
-    return separarCardsData(dadosGet)
+    return separarCardsData(dadosGet);
   } catch (error) {
     console.log("Erro ao fazer o GET", error);
-    return []
+    return [];
   }
 };
 
+export const listar = async () => {
+  try {
+    const response = await axios.get(
+      "https://6722c0392108960b9cc576fc.mockapi.io/vacina"
+    );
+    console.log("Resposta API", response.data);
+    const dadosGet = response.data;
+    return cardPagesData(dadosGet);
+  } catch (error) {
+    console.log("Erro ao fazer o GET", error);
+    return [];
+  }
+};
 
 const separarCardsData = (dadosGet: Cards[]) => {
-const cardsData = dadosGet.map((card: Cards) => ({
-  rua: card.rua,
-  bairro: card.bairro,
-  tipo: card.tipo,
-  numero: card.numero,
-  dataInicio: card.dataInicio,
-  dataFim: card.dataFim,
-  id: card.id
-}));
+  const cardsData = dadosGet.map((card: Cards) => ({
+    rua: card.rua,
+    bairro: card.bairro,
+    tipo: card.tipo,
+    numero: card.numero,
+    dataInicio: card.dataInicio,
+    dataFim: card.dataFim,
+    id: card.id,
+  }));
+  const info: Cards[] = cardsData;
+  return info;
+};
 
-const info:Cards[] = cardsData
-return info
+const cardPagesData = (dadosGet: DataCardPage[]) => {
+  const data = dadosGet.map((data: DataCardPage) => ({
+    rua: data.rua,
+    numero: data.numero,
+    bairro: data.bairro,
+    descricao: data.descricao,
+    dataInicio: data.dataInicio,
+    dataFim: data.dataFim,
+    horaInicio: data.horaInicio,
+    horaFim: data.horaFim,
+    tipo: data.tipo,
+    id: data.id,
+  }));
 
-}
+  const info: DataCardPage[] = data;
+  return info;
+};
 
-
-export const deletarApi = async (id:string) => {
+export const deletarApi = async (id: string) => {
   try {
     const response = await axios.delete(
       `https://6722c0392108960b9cc576fc.mockapi.io/vacina/${id}`
@@ -70,4 +82,4 @@ export const deletarApi = async (id:string) => {
   } catch (error) {
     console.log("Erro ao fazer o Delete", error);
   }
-}
+};
